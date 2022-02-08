237be076140ca92ee3776f8030c2edf3d20f34f7
Chukwuemeka Maduekwe <chukwuemeka@viewcrunch.com>
2022-02-08 22:34:34 +0100

    removed some unused server code

--- utils/fetcher.js
+++ utils/fetcher.js
@@ -3,16 +3,7 @@ import axios from "axios";
 const mode = process.env.NODE_ENV === "production" ? "https://transearch.herokuapp.com/" : "http://localhost:5000/";
 
 const API = async (method = "post", path, payload) => {
-  return await axios[method](
-    `${mode}${path}`,
-    payload
-    // {
-    //   headers: {
-    //     "Content-Type": "application/json;charset=UTF-8",
-    //     "Access-Control-Allow-Origin": "*",
-    //   },
-    // }
-  )
+  return await axios[method](`${mode}${path}`, payload)
     .then((res) => res.data)
     .catch((err) => {
       console.log("AXIOS ERROR: ", err);
